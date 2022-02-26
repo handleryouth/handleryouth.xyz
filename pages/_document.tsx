825a960445cb609277212739da6fec1f196a3d8a
Tony <rafaeltonydavid@yahoo.com>
2022-02-27 00:00:03 +0700

    chore: configure prettier

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,4 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-} from "next/document";
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -14,109 +8,41 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-          <link
-            rel="stylesheet"
-            href="https://unpkg.com/primeicons/primeicons.css"
-          />
+          <link rel="stylesheet" href="https://unpkg.com/primeicons/primeicons.css" />
           <link
             rel="stylesheet"
             href="https://unpkg.com/primereact/resources/themes/lara-light-indigo/theme.css"
           />
-          <link
-            rel="stylesheet"
-            href="https://unpkg.com/primereact/resources/primereact.min.css"
-          />
-          <link
-            rel="stylesheet"
-            href="https://unpkg.com/primeflex@2.0.0/primeflex.min.css"
-          />
-          <link
-            rel="preload"
-            href="/api/project"
-            as="fetch"
-            crossOrigin="anonymous"
-          />
+          <link rel="stylesheet" href="https://unpkg.com/primereact/resources/primereact.min.css" />
+          <link rel="stylesheet" href="https://unpkg.com/primeflex@2.0.0/primeflex.min.css" />
+          <link rel="preload" href="/api/project" as="fetch" crossOrigin="anonymous" />
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="language" content="English" />
           <meta name="author" content="Tony David" />
-          <link
-            rel="apple-touch-icon"
-            sizes="57x57"
-            href="/icons/apple-icon-57x57.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="60x60"
-            href="/icons/apple-icon-60x60.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="72x72"
-            href="/icons/apple-icon-72x72.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="76x76"
-            href="/icons/apple-icon-76x76.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="114x114"
-            href="/icons/apple-icon-114x114.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="120x120"
-            href="/icons/apple-icon-120x120.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="144x144"
-            href="/icons/apple-icon-144x144.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="152x152"
-            href="/icons/apple-icon-152x152.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/icons/apple-icon-180x180.png"
-          />
+          <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png" />
           <link
             rel="icon"
             type="image/png"
             sizes="192x192"
             href="/icons/android-icon-192x192.png"
           />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/icons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="96x96"
-            href="/icons/favicon-96x96.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/icons/favicon-16x16.png"
-          />
+          <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
           <link rel="manifest" href="/icons/manifest.json" />
           <meta name="msapplication-TileColor" content="#4285f4" />
-          <meta
-            name="msapplication-TileImage"
-            content="/icons/ms-icon-144x144.png"
-          />
+          <meta name="msapplication-TileImage" content="/icons/ms-icon-144x144.png" />
           <meta name="theme-color" content="#4285f4" />
         </Head>
         <body>
