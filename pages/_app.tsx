f7abea5205ca3e138b0458a7e89282941e53cdad
Tony <rafaeltonydavid@yahoo.com>
2022-03-10 18:36:58 +0700

    style(_app.tsx): implement inter font-family

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,14 @@ import { Provider } from 'react-redux';
 
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
-import '@fontsource/inter/variable.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import '../styles/globals.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/800.css';
+import '@fontsource/inter/700.css';
+import '@fontsource/inter/600.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const client = new ApolloClient({
