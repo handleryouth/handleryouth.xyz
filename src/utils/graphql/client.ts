e23c23448ad91674dfc226ecdebb24d142c749c4
Tony <rafaeltonydavid@yahoo.com>
2022-05-16 10:33:03 +0700

    refactor(graphqlclient): change apollo client uri

--- src/utils/graphql/client.ts
+++ src/utils/graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: `https://7qohjn-4000.sse.codesandbox.io/graphql`,
+  uri: `https://csb-7qohjn-frwrbf84r-handleryouth.vercel.app/graphql`,
   cache: new InMemoryCache({
     addTypename: false,
   }),
